934a033e6a907c711b6aa99e077605e374a4c89b
igorgo-hyuna <igor.gologots@hyuna.bb>
2021-03-18 11:11:52 +0200

    Finished 51-th lesson

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import s from './users.module.css';
+import * as axios from 'axios';
+import undefinedUserPhoto from '../../assets/images/undefined-user.png';
 
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://www.rewizor.ru/files/1960668x8w.jpg',
-                    followed: true,
-                    fullName: 'Ihor',
-                    status: 'I am a boss',
-                    location: {city: 'Kremenchug', country: 'Ukraine'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://www.rewizor.ru/files/1960668x8w.jpg',
-                    followed: false,
-                    fullName: 'Anna',
-                    status: 'doctor',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://www.rewizor.ru/files/1960668x8w.jpg',
-                    followed: false,
-                    fullName: 'Andrey',
-                    status: 'front-end developer',
-                    location: {city: 'Kyiv', country: 'Ukraine'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://www.rewizor.ru/files/1960668x8w.jpg',
-                    followed: true,
-                    fullName: 'Emil',
-                    status: 'HTML developer',
-                    location: {city: 'Backu', country: 'Azerbaijan'}
-                }
-            ]
-        )
+        //response - можно прмониторить через debugger, что бы увидеть, что он возвращает
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
         return <div className={s.usersContainer}>
@@ -47,17 +19,17 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id} className={s.__item}>
                         <div className={s._topContainer}>
                             <div>
-                                <img src={u.photoUrl} alt=""/>
+                                <img src={u.photos.small != null ? u.photos.small : undefinedUserPhoto} alt=""/>
                             </div>
                         </div>
                         <div className={s._bottomContainer}>
                             <div>
-                                <span>{u.fullName}. </span>
+                                <span>{u.name}. </span>
                                 <span>{u.status}</span>
                             </div>
                             <div>
-                                <span>{u.location.city}, </span>
-                                <span>{u.location.country}</span>
+                                <span>{"u.location.city"}, </span>
+                                <span>{"u.location.country"}</span>
                             </div>
                             <div>
                                 {/* Тернарное выражение */}
